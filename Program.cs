e17e6cad6886da0a280e02e84ff8316b2c7cd802
Dog43y <71520770+DogeDoge17@users.noreply.github.com>
2025-05-29 22:07:47 -0500

    theres HOW many twins??

---
+++ Program.cs
@@ -0,0 +1,175 @@
+﻿using cli_bot;
+using Quill;
+using Quill.Pages;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Path = cli_bot.Path;
+
+
+internal class Program
+{
+    static List<Path> _imagePaths = [];
+    static List<int> _allowedImages = new();
+
+    static List<Path> GetAllFilesRecursive(string rootFolder)
+    {
+        List<Path> files = new(3000);
+        void Search(string folder)
+        {
+            foreach (var file in Directory.GetFiles(folder))
+            {
+                files.Add(file);
+            }
+            foreach (var dir in Directory.GetDirectories(folder))
+            {
+                Search(dir);
+            }
+        }
+        Search(rootFolder);
+        return files;
+    }
+
+    private static void Main(string[] args)
+    {
+        _imagePaths = GetAllFilesRecursive(Path.Assembly / "Nino Nakano Screenshots");
+
+        RefreshWords();
+
+        DriverCreation.SetBrowserType(BrowserType.Firefox);
+        DriverCreation.options.headless = true;
+        TwitterBot nino = new(TimeSpan.FromMinutes(60)) { DisplayName = "Nino Nakano" };
+        nino.runAction += Run;
+        nino.Start();
+    }
+
+    static void RefreshWords(int exclude = -1)
+    {
+        string saidPath = Path.Assembly / "said.txt";
+
+        if (!File.Exists(saidPath))
+        {
+            File.WriteAllText(saidPath, "");
+        }
+
+        string[] lines = File.ReadAllLines(saidPath);
+
+        HashSet<int> badWords = new(lines.Length);
+
+        foreach (string line in lines)
+        {
+            if (line.Trim() != string.Empty)
+            {
+                badWords.Add(Int32.Parse(line));
+            }
+        }
+
+        _allowedImages = new();
+
+        bool safed = false;
+    safety:
+        if (badWords.Count == _imagePaths.Count || safed)
+        {
+            badWords = new();
+            SaveBlacklist();
+        }
+
+        for (int i = 0; i < _imagePaths.Count; i++)
+        {
+            if (badWords.Contains(i) || i == exclude)
+                continue;
+            _allowedImages.Add(i);
+        }
+
+        if (_allowedImages.Count == 0)
+        {
+            safed = true;
+            goto safety;
+        }
+    }
+
+    static void SaveBlacklist()
+    {
+        HashSet<int> allowed = new(_allowedImages);
+
+        StringBuilder outp = new StringBuilder();
+
+        for (int i = 0; i < _imagePaths.Count; i++)
+        {
+            if (!allowed.Contains(i))
+                outp.AppendLine(i.ToString());
+        }
+
+        File.WriteAllText(Path.Assembly / "said.txt", outp.ToString());
+    }
+
+    public static string GenerateContent(Path path)
+    {
+        if (path.Contains("Movie" + Path.GoodSep))
+            return "Movie";
+
+        if (path.Contains("Extra Art"))
+            return "Extra Art";
+
+        if (path.Contains("Extra") || path.Contains("Cover"))
+        {
+            if (path.Contains("Manga"))
+                return path.ParentPath.ParentPath.FileName;
+        }
+
+        if (path.Contains("OVA") && path.Contains("Episode"))
+        {
+            int num = path.Contains("OVA 1") ? 1 : 2;
+
+            return $"OVA {num} {path.ParentPath.FileName.Replace("Episode ", "E")}";
+        }
+
+        return path.ParentPath.FileName;
+    }
+
+    [DoesNotReturn]
+    private static void TestContentNames()
+    {
+        HashSet<string> said = new();
+        string digestiblePath = Path.Assembly / "name_output_digestible.txt";
+        string fullPath = Path.Assembly / "name_output_full.txt";
+
+        using (var writerDigestible = new StreamWriter(digestiblePath.ToString(), false))
+        using (var writerFull = new StreamWriter(fullPath.ToString(), false))
+        {
+            for (int i = 0; i < _imagePaths.Count; i++)
+            {
+                Path trimmedPath = _imagePaths[i].RelativeTo(Path.Assembly);
+                string content = GenerateContent(trimmedPath);
+
+                writerFull.WriteLine($"{trimmedPath} => {content}");
+
+                if (said.Contains(content))
+                    continue;
+                said.Add(content);
+                writerDigestible.WriteLine($"{trimmedPath} => {content}");
+            }
+        }
+        Environment.Exit(0);
+    }
+
+    static void Run(ComposePage composer, string[] args)
+    {
+        try
+        {
+            int rndTweet = Random.Shared.Next(0, _allowedImages.Count);
+            Path imagePath = _imagePaths[_allowedImages[rndTweet]];
+
+            Output.WriteLine($"Tweeting \"{imagePath}\"");
+            composer.Tweet(GenerateContent(imagePath.RelativeTo(Path.Assembly)), imagePath);
+            RefreshWords(rndTweet);
+            SaveBlacklist();
+            Output.WriteLine($"Finished");
+
+        }
+        catch (Exception ex)
+        {
+            Output.WriteLine(ex.Message);
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Nino Nakano Screenshot Bot
+ The source code to [Nino Nakano Screenshot Bot](https://x.com/BestNinoBot) on Twitter\
+ Images are not supplied in this repo. They can be found in the [Google Drive folder](https://drive.google.com/drive/u/0/folders/1psNDJ_qd7O62TBYiJldYgCMMFu3nF5sl). Download the folder as a zip and place "Nino Nakano Screenshots" into the root directory of the project.
+
+# Setup
+ [Build](https://github.com/Doge-Productions/Quill.api/tree/experimental) or [locate Quill.dll](https://github.com/DogeDoge17/cli-bot-lib/blob/main/Quill.dll) and [build](https://github.com/DogeDoge17/cli-bot-lib), and then make project references to both the libraries in the csproj. You may also recursive clone [cli-bot-lib](https://github.com/DogeDoge17/cli-bot-lib) and everything should work from there (granted you don't change the structure).\
+ Make a login.txt file in the main directory file and fill it in this manner
+```bash
+username
+password
+```
+ It's not the most secure method to do this but it'll have to work with me.
+# Dependencies
+ You can find the source of the cli-bot dll at [cli-bot-lib](https://github.com/DogeDoge17/cli-bot-lib)\
+ You can find the Quill.dll file inside the [cli-bot-lib](https://github.com/DogeDoge17/cli-bot-lib) repo as its required or you may compile the binary from [here](https://github.com/Doge-Productions/Quill.api/tree/experimental).\
+ You also need [Newtonsoft.Json](https://www.nuget.org/packages/newtonsoft.json/), [Selenium](https://www.nuget.org/packages/selenium.webdriver), and [WaitHelpers](https://www.nuget.org/packages/SeleniumExtras.WaitHelpers) as Quill.dll depends on them.
+# cli-bot-lib
+ Again, this repo does not include most of the code that runs the management of the bot, but only what will be tweeted. All that code can be found in [cli-bot-lib](https://github.com/DogeDoge17/cli-bot-lib). Please refer to that if you want to see more of the innerworkings of the bot. 
+# Quill.dll
+  Quill is a library I wrote that streamlines webscraping twitter with the sole purpose of making bots. It's limited in features as its only good for composing tweets but that should be enough to satisfy the needs of most bots. There is no nuget page setup as of right now so the only way to obtain the DLL is by compiling the [experimental branch](https://github.com/Doge-Productions/Quill.api/tree/experimental) or by downloading the precompiled binary from [here](https://github.com/DogeDoge17/cli-bot-lib/blob/main/Quill.dll).
